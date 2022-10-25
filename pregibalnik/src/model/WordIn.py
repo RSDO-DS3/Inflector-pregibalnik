1e8f6b88e016f4e72ef90380443bd165570aacda
Jaka Čibej <35957765+jakacibej@users.noreply.github.com>
2022-10-25 08:43:45 +0200

    Update WordIn.py
    Added "patternCode: Optional[str]"

--- pregibalnik/src/model/WordIn.py
+++ pregibalnik/src/model/WordIn.py
@@ -1,6 +1,9 @@
+from typing import Optional
+
 from pydantic import BaseModel, constr
 
 
 class WordIn(BaseModel):
     lema: constr(min_length=1)
     msd: constr(min_length=1)
+    patternCode: Optional[str]
